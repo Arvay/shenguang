be56d2158e0d0379717b5e03b7548fa27480debc
Ghost <17600079797@163.com>
2020-01-02 18:01:27 +0800

    增加备案信息

--- shenguang/src/components/HelloWorld.vue
+++ shenguang/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <img style="width: 100%" src="./img/01.png" alt="">
+    <div class="code" @click="open">沪ICP备19026161号-1</div>
   </div>
 </template>
 
@@ -11,24 +12,38 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods: {
+    open () {
+      window.open('http://www.beian.miit.gov.cn')
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .code a {
+    font-size: 14px;
+    line-height: 40px;
+    color: #666666;
+  }
 </style>
